1218f2e0b78b7b78f17369130b52df2957b308f9
stackmodechris-droid <stackmodechris@gmail.com>
2026-06-15 12:46:57 -0400

    chore: update robots.ts to explicitly include Googlebot and Googlebot-Image

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
-        userAgent: "*",
+        userAgent: ["*", "Googlebot", "Googlebot-Image"],
         allow: "/",
         disallow: ["/api/", "/_next/", "/admin/"],
       },
